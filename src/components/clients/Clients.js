da0c7326aaf52e083e6b49ee5f0bff71c7f78443
Mario Quintana <pyrros2000@gmail.com>
2018-12-28 12:46:15 -0800

    Created first client to display on a table

---
+++ src/components/clients/Clients.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Clients extends Component {
+  render() {
+    const clients = [{
+      id: '555550',
+      firstName: 'Kevin',
+      lastName: 'John',
+      email: 'kev@yahoo.com',
+      phone: '881-96-4561',
+      balance: '35'
+    }]
+
+    if(clients){
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2><i className="fas fa-users">Clients</i></h2>
+            </div>
+            <div className="col-md-6"/>
+          </div>
+
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th/>
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>{client.firstName} {client.lastName}</td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link to={`/client/${client.id}`} className="btn btn-secondary btn-sm">
+                      <i className="fas fa-arrow-circle-right"> Details</i>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )
+    } else {
+      return <h1>Loading...</h1>
+    }
+  }
+}
+
+export default Clients;
