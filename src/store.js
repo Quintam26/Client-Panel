b4cf519e1b1593c19725315b859bf5916fa99b20
Mario Quintana <pyrros2000@gmail.com>
2018-12-29 15:27:16 -0800

    Creating more actions and reducers

--- src/store.js
+++ src/store.js
@@ -5,6 +5,7 @@ import { reactReduxFirebase, firebaseReducer } from "react-redux-firebase";
 import { reduxFirestore, firestoreReducer } from "redux-firestore";
 //Reducers
 import notifyReducer from './reducers/notifyReducer';
+import settingsReducer from './reducers/settingsReducer';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDUlga09tddXCMvKt1h6CepzsayD7BAZHI",
@@ -38,7 +39,8 @@ const createStoreWithFirebase = compose(
 const rootReducer = combineReducers({
   firebase: firebaseReducer,
   firestore: firestoreReducer, // <- needed if using firestore
-  notify: notifyReducer
+  notify: notifyReducer,
+  settings: settingsReducer
 });
 
 //Create initial state
