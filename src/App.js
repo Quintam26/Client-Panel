9491ded3e08f69a4f2d2ae7f57ee9f2e30bec7ef
Mario Quintana <pyrros2000@gmail.com>
2018-12-29 21:14:24 -0800

    Created settings for user that saves in LocalStorage

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { UserIsAuthenticated, UserIsNotAuthenticated } from './helpers/auth';
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./components/layout/Dashboard";
 import Login from "./components/auth/Login";
+import Settings from "./components/settings/Settings";
 import AddClient from "./components/clients/AddClient";
 import ClientDetails from "./components/clients/ClientDetails";
 import { Provider } from "react-redux";
@@ -21,6 +22,7 @@ class App extends Component {
             <div className="container">
               <Switch>
                 <Route exact path="/" component={UserIsAuthenticated(Dashboard)} />
+                <Route exact path="/settings" component={UserIsAuthenticated(Settings)} />
                 <Route exact path="/login" component={UserIsNotAuthenticated(Login)} />
                 <Route exact path="/client/add" component={UserIsAuthenticated(AddClient)} />
                 <Route exact path="/client/edit/:id" component={UserIsAuthenticated(EditClient)} />
--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -1,29 +1,23 @@
 import { DISABLE_BALANCE_ON_ADD, DISABLE_BALANCE_ON_EDIT, ALLOW_REGISTERATION } from '../actions/types';
 
-const initialState = {
-  disableBalanceOnAdd: true,
-  disableBalanceOnEdit: false,
-  allowRegisteration: false
-};
-
-export default function(state= initialState, action) {
+export default function(state = {}, action) {
   switch(action.type) {
     case DISABLE_BALANCE_ON_ADD:
       return {
         ...state,
-        disableBalanceOnAdd: !state.disableBalanceOnAdd
+        disableBalanceOnAdd: action.payload
       };
     case DISABLE_BALANCE_ON_EDIT:
       return {
         ...state,
-        disableBalanceOnEdit: !state.disableBalanceOnEdit
+        disableBalanceOnEdit: action.payload
       };
     case ALLOW_REGISTERATION:
       return {
         ...state,
-        allowRegisteration: !state.allowRegisteration
+        allowRegisteration: action.payload
       };
     default: 
-    return state;
+      return state;
   }
 };
